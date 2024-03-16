3a4b7282f29bbb7b0ad4d13fa67ca0234647e51a
Mateo Betancur B <mateobetancurb@gmail.com>
2024-03-15 20:15:01 -0500

    Add environment configuration file

---
+++ src/config/env.js
@@ -0,0 +1,5 @@
+export const env = {
+	TMDB_HOSTNAME: process.env.TMDB_HOSTNAME || "",
+	THE_MOVIES_DATABASE_API_KEY: process.env.THE_MOVIES_DATABASE_API_KEY || "",
+	ACCESS_TOKEN_API: process.env.ACCESS_TOKEN_API || "",
+};
