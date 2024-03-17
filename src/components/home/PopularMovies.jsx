64bc33ac5842adbf26014a80b1c9fce0c4d8ff49
Mateo Betancur B <mateobetancurb@gmail.com>
2024-03-17 14:55:41 -0500

    Add PopularMovies component to display popular movies in a carousel

---
+++ src/components/home/PopularMovies.jsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from "@/components/ui/carousel";
+
+import { getPopularMovies } from "@/services/movies";
+
+const PopularMovies = async () => {
+	const popularMoviesList = await getPopularMovies();
+	const imageDomainUrl = "https://image.tmdb.org/t/p/w500";
+
+	return (
+		<section>
+			<Carousel>
+				<CarouselContent>
+					{popularMoviesList.map((movie) => (
+						<CarouselItem key={movie.id}>
+							<h2>{movie.original_title} </h2>
+							<Image
+								src={`${imageDomainUrl}${movie.poster_path}`}
+								alt={movie.original_title}
+								width={200}
+								height={200}
+							/>
+						</CarouselItem>
+					))}
+				</CarouselContent>
+				<CarouselPrevious />
+				<CarouselNext />
+			</Carousel>
+		</section>
+	);
+};
+
+export default PopularMovies;
