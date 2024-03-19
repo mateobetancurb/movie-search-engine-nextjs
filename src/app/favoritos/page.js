9e7e1351e5e9ece0d858e62547c8370488c590c6
Mateo Betancur B <mateobetancurb@gmail.com>
2024-03-19 11:27:48 -0500

    Add favorites page with metadata and basic structure

---
+++ src/app/favoritos/page.js
@@ -0,0 +1,13 @@
+export const metadata = {
+	title: "Tus favoritos",
+	description:
+		"Aquí podrás guardar tus programas de TV, películas o series favoritas",
+};
+
+export default function Favorites() {
+	return (
+		<>
+			<h2>favoritos</h2>
+		</>
+	);
+}
