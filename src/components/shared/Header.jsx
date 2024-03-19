dd7b353e4ef7699709456d6ad5ff1e5ff434e8dd
Mateo Betancur B <mateobetancurb@gmail.com>
2024-03-19 10:56:34 -0500

    Add Header component

---
+++ src/components/shared/Header.jsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+	return (
+		<header className="text-center mb-10 space-y-3">
+			<h1 className="mt-10 text-3xl font-bold">Movies.com</h1>
+			<h2>Busca tus películas favoritas y comienza a verlas</h2>
+		</header>
+	);
+};
