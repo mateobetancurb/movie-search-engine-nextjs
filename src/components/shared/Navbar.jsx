60d3e3bd25a3ff3714ce118a01a5099adfb7d367
Mateo Betancur B <mateobetancurb@gmail.com>
2024-03-19 10:56:40 -0500

    Add Navbar component

---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+const navbarItems = [
+	{ path: "peliculas", name: "Películas" },
+	{ path: "favoritos", name: "Mis favoritos" },
+];
+
+export const Navbar = () => {
+	return (
+		<nav className="flex bg-slate-600 text-white py-5 justify-center gap-10">
+			{navbarItems.map((item) => (
+				<Link
+					key={item.path}
+					href={item.path}
+					className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+				>
+					{item.name}
+				</Link>
+			))}
+		</nav>
+	);
+};
