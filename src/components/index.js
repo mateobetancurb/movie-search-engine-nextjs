93d35cb541b5524c3a99fdc8655d31cdcac3fe07
Mateo Betancur B <mateobetancurb@gmail.com>
2024-03-19 10:56:13 -0500

    Add new components to index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,11 @@
+// home
 export * from "./home/PopularMovies";
+
+// shared
+export * from "./shared/Header";
+export * from "./shared/Navbar";
+export * from "./shared/Footer";
+
+// ui
 export * from "./ui/button";
 export * from "./ui/carousel";
