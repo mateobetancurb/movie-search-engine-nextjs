af95031e9cbbf088443766ea132eac75489aedfb
Mateo Betancur B <mateobetancurb@gmail.com>
2024-03-24 13:57:27 -0500

    Update header color to white

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export const Header = () => {
 	return (
-		<header className="text-center mb-10 space-y-3">
+		<header className="text-center mb-10 space-y-3 text-white">
 			<Link href="/">
 				<h1 className="mt-10 text-3xl font-bold">Movies.com</h1>
 			</Link>
