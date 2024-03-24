a1d278c73ea48d7ec49fdfa83c7912e3bf6b0281
Mateo Betancur B <mateobetancurb@gmail.com>
2024-03-24 13:56:59 -0500

    Update body background color

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="es">
-			<body>
+			<body className="bg-neutral-800">
 				<Header />
 				<Navbar />
 				{children}
