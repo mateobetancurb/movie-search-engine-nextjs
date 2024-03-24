a0d0f22f3c51a00e5855bc792ae0e1b396a49c0f
Mateo Betancur B <mateobetancurb@gmail.com>
2024-03-24 13:57:44 -0500

    Add NavbarItem client component

---
+++ src/components/shared/NavbarItem.jsx
@@ -0,0 +1,19 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export const NavbarItem = ({ path, name }) => {
+	const pathName = usePathname();
+	return (
+		<>
+			<Link
+				key={path}
+				href={path}
+				className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+			>
+				{name}
+			</Link>
+		</>
+	);
+};
