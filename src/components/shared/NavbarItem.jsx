d50b0096033662b68c9f5002170217fb57b2b275
Mateo Betancur B <mateobetancurb@gmail.com>
2024-03-24 14:22:24 -0500

    Refactor NavbarItem component styling

--- src/components/shared/NavbarItem.jsx
+++ src/components/shared/NavbarItem.jsx
@@ -10,7 +10,9 @@ export const NavbarItem = ({ path, name }) => {
 			<Link
 				key={path}
 				href={path}
-				className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+				className={`block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground ${
+					path === pathName ? "bg-neutral-200 text-black" : ""
+				}`}
 			>
 				{name}
 			</Link>
