71d4c58f8ae86d6d293fa34a20751b309c5e489c
Mateo Betancur B <mateobetancurb@gmail.com>
2024-03-29 12:41:11 -0500

    Add error handling and logging for movie details retrieval

--- src/services/movies.js
+++ src/services/movies.js
@@ -11,9 +11,37 @@ export const getPopularMovies = async () => {
 				Authorization: `Bearer ${ACCESS_TOKEN_API}`,
 			},
 		});
+
+		if (!req.ok) {
+			throw new Error(`HTTP error ${req.status}`);
+		}
+
 		const { results } = await req.json();
 		return results;
 	} catch (error) {
-		console.log(error);
+		console.error("Error al obtener detalles de la película:", error);
+		throw error;
+	}
+};
+
+export const getMovieDetailsById = async (id) => {
+	try {
+		const ACCESS_TOKEN_API = env.ACCESS_TOKEN_API;
+		const req = await fetch(`${TMDB_URLS.movies.findById}/${id}`, {
+			headers: {
+				accept: "application/json",
+				Authorization: `Bearer ${ACCESS_TOKEN_API}`,
+			},
+		});
+
+		if (!req.ok) {
+			throw new Error(`HTTP error ${req.status}`);
+		}
+
+		const results = await req.json();
+		return results;
+	} catch (error) {
+		console.error("Error al obtener detalles de la película:", error);
+		throw error;
 	}
 };
