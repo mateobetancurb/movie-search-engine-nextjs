9bc989706d9e75b2371f1493a6d71a3fb3baa4f8
Mateo Betancur B <mateobetancurb@gmail.com>
2024-03-29 12:41:38 -0500

    Add findById URL to movies object in TMDB_URLS

--- src/services/urls.js
+++ src/services/urls.js
@@ -3,5 +3,6 @@ import { env } from "@/config/env";
 export const TMDB_URLS = {
 	movies: {
 		popular: `${env.TMDB_HOSTNAME}/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`,
+		findById: `${env.TMDB_HOSTNAME}/movie`,
 	},
 };
