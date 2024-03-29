7e3fe08b5f217ffc2a63c8ef02456f26bc393b41
Mateo Betancur B <mateobetancurb@gmail.com>
2024-03-29 10:06:06 -0500

    Add NotFound 404 component

---
+++ src/app/not-found.jsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function NotFound() {
+	return (
+		<main className="relative w-full mt-20 flex flex-col justify-center items-center bg-neutral-800">
+			<h1 className="text-9xl mb-20 font-extrabold text-white tracking-widest">
+				404
+			</h1>
+			<div className="bg-neutral-600 font-bold text-white p-2 text-sm rounded rotate-12 top-32 absolute">
+				Lo que estás buscando no existe
+			</div>
+			<Link
+				href={"/"}
+				className="px-8 py-3 rounded-lg bg-neutral-600 text-white border hover:bg-neutral-700 transition-all"
+			>
+				Volver
+			</Link>
+		</main>
+	);
+}
