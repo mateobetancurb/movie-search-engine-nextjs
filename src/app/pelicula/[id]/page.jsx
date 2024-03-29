af5124d0b702175859a578b6884a5ca14eba095e
Mateo Betancur B <mateobetancurb@gmail.com>
2024-03-29 13:38:25 -0500

    Add MovieDetails component to MoviePage

--- src/app/pelicula/[id]/page.jsx
+++ src/app/pelicula/[id]/page.jsx
@@ -1,6 +1,11 @@
 import { getMovieDetailsById } from "@/services/movies";
+import { MovieDetails } from "@/components";
 
 export default async function MoviePage({ params }) {
 	const movieDetails = await getMovieDetailsById(params.id);
-	return <>{JSON.stringify(movieDetails)}</>;
+	return (
+		<>
+			<MovieDetails details={movieDetails} />
+		</>
+	);
 }
