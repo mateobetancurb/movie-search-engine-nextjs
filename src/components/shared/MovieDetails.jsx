f2e5cfd232e8a9116a9db9b4a2bac0402a58ffb8
Mateo Betancur B <mateobetancurb@gmail.com>
2024-03-29 19:49:56 -0500

    Add background image to MovieDetails component

--- src/components/shared/MovieDetails.jsx
+++ src/components/shared/MovieDetails.jsx
@@ -1,3 +1,16 @@
 export const MovieDetails = ({ details }) => {
-	return <>{JSON.stringify(details)}</>;
+	const imageDomainUrl = "https://image.tmdb.org/t/p/w500";
+	return (
+		<>
+			<section
+				style={{
+					backgroundImage: `url(${imageDomainUrl}${details.backdrop_path})`,
+					backgroundSize: "cover",
+					backgroundPosition: "center",
+					height: "100vh",
+					opacity: 0.05,
+				}}
+			></section>
+		</>
+	);
 };
