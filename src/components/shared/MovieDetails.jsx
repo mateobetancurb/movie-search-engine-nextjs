df80d1c57172c2adcf1c9e7ccaa6b05225fd6edc
Mateo Betancur B <mateobetancurb@gmail.com>
2024-03-29 22:34:56 -0500

    Refactor MovieDetails component layout and add lorem ipsum text

--- src/components/shared/MovieDetails.jsx
+++ src/components/shared/MovieDetails.jsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Link from "next/link";
 
 export const MovieDetails = ({ details }) => {
 	const imageDomainUrl = "https://image.tmdb.org/t/p/w500";
@@ -12,8 +11,8 @@ export const MovieDetails = ({ details }) => {
 				}}
 				className="bg-cover bg-center h-screen absolute top-0 left-0 right-0 bottom-0 z-10"
 			/>
-			<main className="relative top-14 bg-black rounded-xl w-2/3 p-5 items-center mx-auto h-[500px] text-white">
-				<section className="flex gap-5">
+			<main className="relative top-14 bg-black rounded-xl w-2/3 p-5 items-center mx-auto h-fit text-white">
+				<section className="flex gap-5 mb-5">
 					<div className="flex-1">
 						<Image
 							src={`${imageDomainUrl}${details.backdrop_path}`}
@@ -21,16 +20,30 @@ export const MovieDetails = ({ details }) => {
 							priority={false}
 							width={900}
 							height={900}
-							className="rounded-xl w-full h-full object-cover"
+							className="rounded-xl object-cover"
 						/>
 					</div>
-					<div className="flex-1 flex flex-col justify-center mx-auto text-center">
+					<div className="flex-1 flex flex-col justify-center">
 						<h2 className="text-center font-bold text-2xl">
 							{details.original_title}
 						</h2>
 						<p>{details.overview}</p>
 					</div>
 				</section>
+				<p className="text-white ">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+					provident hic temporibus labore? Aliquam soluta cum enim fugit
+					cupiditate laudantium deserunt perferendis repudiandae ratione. Sunt
+					quaerat rerum veritatis doloribus vel porro non dignissimos aliquid
+					maxime fugit nobis itaque delectus, qui repudiandae assumenda eius ad
+					a consequatur incidunt dolorum veniam. Maxime suscipit numquam
+					voluptatibus voluptates animi aliquid autem rem, amet distinctio quos
+					ipsum reiciendis non nostrum similique dicta, sed aspernatur sapiente
+					nobis adipisci! Corporis, libero. Ab hic aliquid pariatur quo
+					voluptatibus autem ea error necessitatibus dolorem corporis laborum
+					numquam, fugit explicabo? Saepe, suscipit mollitia voluptatum ipsam
+					aspernatur rem eligendi repudiandae quae qui fugit?
+				</p>
 			</main>
 		</div>
 	);
