bab2f253f74211c2e9304592f4d7a231ed9e441a
Mateo Betancur B <mateobetancurb@gmail.com>
2024-03-31 09:41:36 -0500

    Add upcoming movies URL to TMDB service

--- src/services/urls.js
+++ src/services/urls.js
@@ -4,5 +4,6 @@ export const TMDB_URLS = {
 	movies: {
 		popular: `${env.TMDB_HOSTNAME}/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`,
 		findById: `${env.TMDB_HOSTNAME}/movie`,
+		upcoming: `${env.TMDB_HOSTNAME}/movie/upcoming`,
 	},
 };
