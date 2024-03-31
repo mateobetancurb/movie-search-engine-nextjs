a018a46a6a263b1cc423f56982f651d0ce0166ee
Mateo Betancur B <mateobetancurb@gmail.com>
2024-03-31 09:40:01 -0500

    Refactor PopularMovies component to use MoviesCarousel component

--- src/components/home/PopularMovies.jsx
+++ src/components/home/PopularMovies.jsx
@@ -1,44 +1,13 @@
-import Image from "next/image";
-import Link from "next/link";
-import {
-	Carousel,
-	CarouselContent,
-	CarouselItem,
-	CarouselNext,
-	CarouselPrevious,
-} from "@/components";
-
+import { MoviesCarousel } from "@/components";
 import { getPopularMovies } from "@/services/movies";
 
 export const PopularMovies = async () => {
 	const popularMoviesList = await getPopularMovies();
-	const imageDomainUrl = "https://image.tmdb.org/t/p/w500";
 
 	return (
 		<section className="mb-10">
-			<h2 className="text-white text-xl mb-5">Películas populares</h2>
-			<Carousel>
-				<CarouselContent>
-					{popularMoviesList.map((movie) => (
-						<CarouselItem
-							key={movie.id}
-							className="md:basis-1/2 lg:basis-1/5 pl-4"
-						>
-							<Link href={`pelicula/${movie.id}`}>
-								<Image
-									src={`${imageDomainUrl}${movie.poster_path}`}
-									alt={movie.original_title}
-									width={200}
-									height={200}
-									priority={true}
-								/>
-							</Link>
-						</CarouselItem>
-					))}
-				</CarouselContent>
-				<CarouselPrevious />
-				<CarouselNext />
-			</Carousel>
+			<h2 className="text-white text-xl mb-5">✨ Películas populares</h2>
+			<MoviesCarousel movies={popularMoviesList} />
 		</section>
 	);
 };
