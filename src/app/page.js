60d3d437df1f4d364d0cc938aa3932a280d74ad3
Mateo Betancur B <mateobetancurb@gmail.com>
2024-03-30 19:23:29 -0500

    Add UpcomingMovies component to HomePage

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import { PopularMovies } from "@/components/home/PopularMovies";
+import { UpcomingMovies } from "@/components/home/UpcomingMovies";
 
 const HomePage = async () => {
 	return (
 		<div className="p-20">
 			<PopularMovies />
+			<UpcomingMovies />
 		</div>
 	);
 };
