0a024b89cface89f048d2a4f6f7a836d24d4a14d
Mateo Betancur B <mateobetancurb@gmail.com>
2024-03-31 09:39:44 -0500

    Add MoviesCarousel component

--- src/components/index.js
+++ src/components/index.js
@@ -6,6 +6,7 @@ export * from "./shared/Header";
 export * from "./shared/Navbar";
 export * from "./shared/Footer";
 export * from "./shared/MovieDetails";
+export * from "./shared/MoviesCarousel";
 
 // shared - client components
 export { NavbarItem } from "./shared/NavbarItem";
