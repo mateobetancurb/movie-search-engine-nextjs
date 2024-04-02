784c35b647aa53daa0fec1fbe9a4c447bef42cb4
Mateo Betancur B <mateobetancurb@gmail.com>
2024-04-01 20:29:25 -0500

    Add toast notification and navigation to favorites page in FavoriteButton component

--- src/components/shared/FavoriteButton.jsx
+++ src/components/shared/FavoriteButton.jsx
@@ -1,14 +1,28 @@
 "use client";
-
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 import useStore from "@/stores/favorites";
 
 export const FavoriteButton = ({ movie }) => {
-	const { favoriteMovies, addMovieToFavorites } = useStore();
+	const { favoriteMovies, addMovieToFavorites, showSonner } = useStore();
+	const router = useRouter();
 
 	const addMovieToFavoriteList = (movie) => {
 		return () => {
 			addMovieToFavorites(movie);
 			console.log(favoriteMovies);
+			toast.success(
+				"Película agregada a favoritos",
+				{
+					action: {
+						label: "Ver",
+						onClick: () => router.push("/favoritos"),
+					},
+				},
+				{
+					position: "top-right",
+				}
+			);
 		};
 	};
 
@@ -18,7 +32,7 @@ export const FavoriteButton = ({ movie }) => {
 				onClick={addMovieToFavoriteList(movie)}
 				className="absolute z-20 shadow-xl bg-neutral-600 px-5 py-1 rounded-xl text-white hover:bg-neutral-700 transition-all"
 			>
-				❤️ Agregar a favoritos
+				Agregar a favoritos ❤️
 			</button>
 		</div>
 	);
