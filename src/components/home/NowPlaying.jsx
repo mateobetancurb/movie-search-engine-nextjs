d23d2675546717b72c9c4d249a2bcc66ea36cf2d
Mateo Betancur B <mateobetancurb@gmail.com>
2024-04-05 11:26:40 -0500

    Add NowPlaying component to HomePage

---
+++ src/components/home/NowPlaying.jsx
@@ -0,0 +1,13 @@
+import { getNowPlayingMovies } from "@/services/movies";
+import { MoviesCarousel } from "@/components";
+
+export const NowPlaying = async () => {
+	const playingMovies = await getNowPlayingMovies();
+
+	return (
+		<section className="mb-10">
+			<h2 className="text-white text-xl mb-5">📺 Las personas están viendo</h2>
+			<MoviesCarousel movies={playingMovies} />
+		</section>
+	);
+};
