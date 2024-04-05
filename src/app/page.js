c2b258a41726c9c0066ee0667e0ba7fc25e15cdb
Mateo Betancur B <mateobetancurb@gmail.com>
2024-04-05 11:25:57 -0500

    Add NowPlaying component to HomePage

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
-import { PopularMovies } from "@/components/home/PopularMovies";
-import { UpcomingMovies } from "@/components/home/UpcomingMovies";
+import { PopularMovies, UpcomingMovies, NowPlaying } from "@/components";
 
 const HomePage = async () => {
 	return (
 		<div className="p-20">
 			<PopularMovies />
 			<UpcomingMovies />
+			<NowPlaying />
 		</div>
 	);
 };
