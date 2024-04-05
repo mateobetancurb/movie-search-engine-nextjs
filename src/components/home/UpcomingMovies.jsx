e2f563fcdeda1297f6cc214776489517de10ccea
Mateo Betancur B <mateobetancurb@gmail.com>
2024-04-05 11:26:45 -0500

    Refactor UpcomingMovies component in HomePage

--- src/components/home/UpcomingMovies.jsx
+++ src/components/home/UpcomingMovies.jsx
@@ -4,11 +4,9 @@ import { MoviesCarousel } from "@/components";
 export const UpcomingMovies = async () => {
 	const upcomingMovies = await getUpcomingMovies();
 	return (
-		<>
-			<section className="mb-10">
-				<h2 className="text-white text-xl mb-5">🍿 Espéralas muy pronto</h2>
-				<MoviesCarousel movies={upcomingMovies} />
-			</section>
-		</>
+		<section className="mb-10">
+			<h2 className="text-white text-xl mb-5">🍿 Espéralas muy pronto</h2>
+			<MoviesCarousel movies={upcomingMovies} />
+		</section>
 	);
 };
