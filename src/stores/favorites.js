bb7d80a415945ce5fea4a76e654da7a78cf25084
Mateo Betancur B <mateobetancurb@gmail.com>
2024-04-09 13:48:49 -0500

    validate if the was already added

--- src/stores/favorites.js
+++ src/stores/favorites.js
@@ -3,7 +3,14 @@ import { create } from "zustand";
 const useStore = create((set) => ({
 	favoriteMovies: [],
 	addMovieToFavorites: (movie) =>
-		set((state) => ({ favoriteMovies: [...state.favoriteMovies, movie] })),
+		set((state) => ({
+			favoriteMovies: state.favoriteMovies.some(
+				(favoriteMovie) => favoriteMovie.id === movie.id
+			)
+				? state.favoriteMovies
+				: [...state.favoriteMovies, movie],
+		})),
+
 	deleteMovieToFavorites: (movie) =>
 		set((state) => ({
 			favoriteMovies: state.favoriteMovies.filter(
