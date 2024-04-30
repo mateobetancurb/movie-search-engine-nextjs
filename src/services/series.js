ed12807547b0250abc52711016427a40daeeffc9
Mateo Betancur B <mateobetancurb@gmail.com>
2024-04-30 13:39:53 -0500

    Add getAiringToday function to fetch TV series airing today from API

---
+++ src/services/series.js
@@ -0,0 +1,25 @@
+import { env } from "@/config/env";
+import { TMDB_URLS } from "./urls";
+
+export const getAiringToday = async () => {
+	try {
+		const ACCESS_TOKEN_API = env.ACCESS_TOKEN_API;
+
+		const req = await fetch(TMDB_URLS.series.airingToday, {
+			headers: {
+				accept: "application/json",
+				Authorization: `Bearer ${ACCESS_TOKEN_API}`,
+			},
+		});
+
+		if (!req.ok) {
+			throw new Error(`HTTP error ${req.status}`);
+		}
+
+		const { results } = await req.json();
+		return results;
+	} catch (error) {
+		console.error("Error al obtener detalles de la serie:", error);
+		throw error;
+	}
+};
