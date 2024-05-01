6fab801830249fbba049994fe5c2a801d66bac2f
Mateo Betancur B <mateobetancurb@gmail.com>
2024-05-01 11:16:46 -0500

    Add getOnTheAir function to fetch TV series currently on air

--- src/services/series.js
+++ src/services/series.js
@@ -23,3 +23,26 @@ export const getAiringToday = async () => {
 		throw error;
 	}
 };
+
+export const getOnTheAir = async () => {
+	try {
+		const ACCESS_TOKEN_API = env.ACCESS_TOKEN_API;
+
+		const req = await fetch(TMDB_URLS.series.onTheAir, {
+			headers: {
+				accept: "application/json",
+				Authorization: `Bearer ${ACCESS_TOKEN_API}`,
+			},
+		});
+
+		if (!req.ok) {
+			throw new Error(`HTTP error ${req.status}`);
+		}
+
+		const { results } = await req.json();
+		return results;
+	} catch (error) {
+		console.error("Error al obtener detalles de la serie:", error);
+		throw error;
+	}
+};
