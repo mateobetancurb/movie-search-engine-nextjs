108aab9a0f2e56d0392591c81d3609093f42abf6
Mateo Betancur B <mateobetancurb@gmail.com>
2024-05-02 11:52:35 -0500

    Add getPopular function to fetch popular TV series

--- src/services/series.js
+++ src/services/series.js
@@ -46,3 +46,26 @@ export const getOnTheAir = async () => {
 		throw error;
 	}
 };
+
+export const getPopular = async () => {
+	try {
+		const ACCESS_TOKEN_API = env.ACCESS_TOKEN_API;
+
+		const req = await fetch(TMDB_URLS.series.popular, {
+			headers: {
+				accept: "application/json",
+				Authorization: `Bearer ${ACCESS_TOKEN_API}`,
+			},
+		});
+
+		if (!req.ok) {
+			throw new Error(`HTTP error ${req.status}`);
+		}
+
+		const { results } = await req.json();
+		return results;
+	} catch (error) {
+		console.error("Error al obtener detalles de la serie:", error);
+		throw error;
+	}
+};
