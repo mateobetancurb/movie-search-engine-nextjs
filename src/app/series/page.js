c4b85bbd316c00745a41d94389804fc823335a55
Mateo Betancur B <mateobetancurb@gmail.com>
2024-05-02 11:56:53 -0500

    Add TopRated component to display top rated TV series

--- src/app/series/page.js
+++ src/app/series/page.js
@@ -1,4 +1,4 @@
-import { AiringToday, OnTheAir, Popular } from "@/components";
+import { AiringToday, OnTheAir, Popular, TopRated } from "@/components";
 
 export const metadata = {
 	title: "Movies.com - Series",
@@ -9,6 +9,7 @@ export default function Series() {
 	return (
 		<>
 			<div className="p-20">
+				<TopRated />
 				<Popular />
 				<AiringToday />
 				<OnTheAir />
