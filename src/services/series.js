396d68c76e39b96d6e7a2fce758d22a6f098c2fe
Mateo Betancur B <mateobetancurb@gmail.com>
2024-05-02 11:57:10 -0500

    Add getTopRated function to fetch top rated TV series

--- src/services/series.js
+++ src/services/series.js
@@ -69,3 +69,26 @@ export const getPopular = async () => {
 		throw error;
 	}
 };
+
+export const getTopRated = async () => {
+	try {
+		const ACCESS_TOKEN_API = env.ACCESS_TOKEN_API;
+
+		const req = await fetch(TMDB_URLS.series.topRated, {
+			headers: {
+				accept: "application/json",
+				Authorization: `Bearer ${ACCESS_TOKEN_API}`,
+			},
+		});
+
+		if (!req.ok) {
+			throw new Error(`HTTP error ${req.status}`);
+		}
+
+		const { results } = await req.json();
+		return results;
+	} catch (error) {
+		console.error("Error al obtener detalles de la serie:", error);
+		throw error;
+	}
+};
